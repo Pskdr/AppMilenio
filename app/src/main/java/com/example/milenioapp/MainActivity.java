6eea558f796ea5a80ac851da67b80a3c38068973
Pskdr <juanca.naranjo03@gmail.com>
2023-09-13 23:49:23 -0500

    feat: añadiendo login y registro

--- app/src/main/java/com/example/milenioapp/MainActivity.java
+++ app/src/main/java/com/example/milenioapp/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.milenioapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnIngresar;
+    public String strDevice = "";
+    private TextView tvRegistro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnIngresar = findViewById(R.id.btnIniciarsesion);
+        tvRegistro = findViewById(R.id.tvRegistro);
+
+        btnIngresar.setOnClickListener(view ->  {
+            Intent intent = new Intent(MainActivity.this, MainMenu.class);
+
+            Bundle miBundle = new Bundle();
+            miBundle.putString("device", strDevice);
+            intent.putExtras(miBundle);
+            startActivity(intent);
+            finish();
+        });
+
+        tvRegistro.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+
+            Bundle miBundle = new Bundle();
+            miBundle.putString("device", strDevice);
+            intent.putExtras(miBundle);
+            startActivity(intent);
+            finish();
+        });
     }
 }
